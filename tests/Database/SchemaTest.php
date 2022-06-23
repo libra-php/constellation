fe46ee115f23cffe3599a9a9f9d2ae92bbb1e817
William Hleucka <william.hleucka@gmail.com>
2022-06-23 09:39:40 -0600

    test(schema): user create blueprint

--- tests/Database/SchemaTest.php
+++ tests/Database/SchemaTest.php
@@ -16,16 +16,52 @@ class SchemaTest extends TestCase
     public function testSchemaCreateBlueprint()
     {
         $schema_create = Schema::create("test", function (
-            Blueprint $blueprint
+            Blueprint $table
         ) {
-            $blueprint->id("id");
-            $blueprint->varchar("email");
-            $blueprint->varchar("name")->nullable();
-            $blueprint->timestamps();
-            $blueprint->primaryKey("id");
+            $table->id("id");
+            $table->varchar("email");
+            $table->varchar("name")->nullable();
+            $table->timestamps();
+            $table->primaryKey("id");
         });
         $this->assertSame(
-            "CREATE TABLE IF NOT EXISTS test (id BIGINT UNSIGNED NOT NULL AUTO INCREMENT, email VARCHAR(255) NOT NULL, name VARCHAR(255), created_at DATETIME(0) NOT NULL, updated_at TIMESTAMP(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (id))",
+            "CREATE TABLE IF NOT EXISTS test (".
+            "id BIGINT UNSIGNED NOT NULL AUTO INCREMENT, ".
+            "email VARCHAR(255) NOT NULL, ".
+            "name VARCHAR(255), ".
+            "created_at DATETIME(0) NOT NULL, ".
+            "updated_at TIMESTAMP(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP, ".
+            "PRIMARY KEY (id))",
+            $schema_create
+        );
+    }
+
+    public function testSchemaUserCreateBlueprint()
+    {
+
+        $schema_create = Schema::create("users", function (
+            Blueprint $table
+        ) {
+            $table->id();
+            $table->uuid("uuid");
+            $table->varchar("name");
+            $table->varchar("email");
+            $table->binary("password", 40);
+            $table->timestamps();
+            $table->unique("email");
+            $table->primaryKey("id");
+        });
+        $this->assertSame(
+            "CREATE TABLE IF NOT EXISTS users (".
+            "id BIGINT UNSIGNED NOT NULL AUTO INCREMENT, ".
+            "uuid BINARY(16) NOT NULL, ".
+            "name VARCHAR(255) NOT NULL, ".
+            "email VARCHAR(255) NOT NULL, ".
+            "password BINARY(40) NOT NULL, ".
+            "created_at DATETIME(0) NOT NULL, ".
+            "updated_at TIMESTAMP(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP, ".
+            "UNIQUE KEY (email), ".
+            "PRIMARY KEY (id))",
             $schema_create
         );
     }
