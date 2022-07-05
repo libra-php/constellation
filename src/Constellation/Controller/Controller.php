3da7a7269bf7a4df6fdc2e4ff42a3b149d936d8a
William Hleucka <william.hleucka@gmail.com>
2022-07-05 14:57:38 -0600

    feat(controller): native render function

--- src/Constellation/Controller/Controller.php
+++ src/Constellation/Controller/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Constellation\Controller;
 
+use Constellation\Routing\Router;
 use Twig\Environment;
 
 /**
@@ -11,5 +12,23 @@ class Controller
 {
     public function __construct(protected Environment $twig)
     {
+    }
+
+    /**
+     * @return string Body of twig template
+     */
+    public function render(string $template, ?array $data = []): string
+    {
+        $payload = [
+            ...$data,
+            // Functions for twig templates
+            "fn" => new class {
+                public function buildRoute($name, ...$vars)
+                {
+                    return Router::buildRoute($name, ...$vars);
+                }
+            },
+        ];
+        return $this->twig->render($template, $payload);
     }
 }
