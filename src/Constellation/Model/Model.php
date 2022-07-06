2d54421d3a72c594bbb3aa3fe0cce677efd33320
William Hleucka <william.hleucka@gmail.com>
2022-07-06 08:51:33 -0600

    refactor(model): action methods

--- src/Constellation/Model/Model.php
+++ src/Constellation/Model/Model.php
@@ -8,13 +8,52 @@ use PDO;
 class Model
 {
     private array $attributes = [];
+    private bool $loaded = false;
+    private DB $db;
     public function __construct(
-        private DB $db,
         private string $table,
         private array $key,
         private ?array $id = null
     ) {
+        $this->db = DB::getInstance();
         $this->loadAttributes();
+    }
+
+    public static function find(?array $id)
+    {
+        $class = static::class;
+        $model = new $class($id);
+        return $model->isLoaded() ? $model : null;
+    }
+
+    public static function create(array $attributes)
+    {
+        $class = static::class;
+        $model = new $class();
+        if ($model->insert($attributes)) {
+            return $model->isLoaded() ? $model : null;
+        }
+        return null;
+    }
+
+    public static function remove(?array $id)
+    {
+        $class = static::class;
+        $model = new $class($id);
+        if ($model && $model->delete()) {
+            return true;
+        }
+        return null;
+    }
+
+    public function refresh()
+    {
+        $this->loadAttributes();
+    }
+
+    public function isLoaded()
+    {
+        return $this->loaded;
     }
 
     public function loadAttributes()
@@ -31,6 +70,7 @@ class Model
                 ->fetchAll(PDO::FETCH_ASSOC);
             if ($result) {
                 $this->attributes = $result[0];
+                $this->loaded = true;
             }
         } else {
             $result = $this->db
@@ -62,16 +102,49 @@ class Model
         return array_values($this->attributes);
     }
 
-    public function update()
+    public function insert(array $attributes)
     {
-        $update_statement = $this->stmt($this->attributeKeys(), ", ");
+        $columns = array_keys($attributes);
+        $values = array_values($attributes);
+        $update_statement = $this->stmt($columns, ", ");
+        $result = $this->db->run(
+            "INSERT INTO $this->table 
+            SET $update_statement",
+            $values
+        );
+        if ($result) $this->refresh();
+        return $result;
+    }
+
+    public function update(array $attributes)
+    {
+        $columns = array_keys($attributes);
+        $values = array_values($attributes);
+        $update_statement = $this->stmt($columns, ", ");
         $where_clause = $this->stmt($this->key, " AND ");
-        return $this->db->run(
+        $result = $this->db->run(
             "UPDATE $this->table 
             SET $update_statement 
             WHERE $where_clause",
-            [...$this->attributeValues(), ...$this->id]
+            [...$values, ...$this->id]
         );
+        if ($result) $this->refresh();
+        return $result;
+    }
+
+    public function delete()
+    {
+        $where_clause = $this->stmt($this->key, " AND ");
+        $result = $this->db->run(
+            "DELETE FROM $this->table 
+            WHERE $where_clause",
+            $this->id
+        );
+        if ($result) {
+            $this->id = null;
+            $this->refresh();
+        }
+        return $result;
     }
 
     public function __get($name)
