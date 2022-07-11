8dbc560b7c2098da0feace30454d4a107d634eec
William Hleucka <william.hleucka@gmail.com>
2022-07-11 13:10:10 -0600

    feat(validate): form rules

--- src/Constellation/Validation/Validate.php
+++ src/Constellation/Validation/Validate.php
@@ -15,6 +15,12 @@ class Validate
         "email" => "%value must be a valid email address",
         "required" => "%field is a required field",
         "match" => "%field does not match",
+        "min_length" => "%field is too short (min length: %rule_extra)",
+        "max_length" => "%field is too long (max length: %rule_extra)",
+        "uppercase" => "%field requires at least %rule_extra uppercase character",
+        "lowercase" => "%field requires at least %rule_extra lowercase character",
+        "symbol" => "%field requires at least %rule_extra symbol character",
+        "reg_ex" => "%field is invalid",
     ];
     public static $errors = [];
 
@@ -36,16 +42,28 @@ class Validate
     {
         foreach ($request_rules as $request_item => $ruleset) {
             $value = $data[$request_item] ?? null;
-            foreach ($ruleset as $rule) {
+            foreach ($ruleset as $rule_raw) {
+                $rule_split = explode("=", $rule_raw);
+                $rule = $rule_split[0];
+                $extra = count($rule_split) == 2
+                   ? $rule_split[1]
+                    : '';
                 $result = match($rule) {
                     'string' => self::isString($value),
                     'email' => self::isEmail($value),
                     'required' => self::isRequired($value),
                     'match' => self::isMatch($data, $request_item, $value),
+                    'min_length' => self::isMinLength($value, $extra),
+                    'max_length' => self::isMaxLength($value, $extra),
+                    'uppercase' => self::isUppercase($value, $extra),
+                    'lowercase' => self::isLowercase($value, $extra),
+                    'symbol' => self::isSymbol($value, $extra),
+                    'reg_ex' => self::regEx($value, $extra),
                 };
                 if (!$result) {
                     self::addError($rule, [
                         "%rule" => $rule,
+                        "%rule_extra" => $extra,
                         "%field" => $request_item,
                         "%value" => $value
                     ]);
@@ -75,5 +93,38 @@ class Validate
         if (!isset($request_data[$item.'_match'])) return false;
         return $request_data[$item.'_match'] === $value;
 
+    }
+
+    public static function isMinLength($value, $min_length)
+    {
+        return strlen($value) >= $min_length;
+    }
+
+    public static function isMaxLength($value, $max_length)
+    {
+        return strlen($value) <= $max_length;
+    }
+
+    public static function isUppercase($value, $count)
+    {
+        preg_match_all("/[A-Z]/", $value, $matches);
+        return !empty($matches[0]) && count($matches) >= $count;
+    }
+
+    public static function isLowercase($value, $count)
+    {
+        preg_match_all("/[a-z]/", $value, $matches);
+        return !empty($matches[0]) && count($matches) >= $count;
+    }
+
+    public static function isSymbol($value, $count)
+    {
+        preg_match_all("/[$&+,:;=?@#|'<>.^*()%!-]/", $value, $matches);
+        return !empty($matches[0]) && count($matches) >= $count;
+    }
+
+    public static function regEx($value, $pattern)
+    {
+        return preg_match("/{$pattern}/", $value);
     }
 }
